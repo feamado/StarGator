4ac32f1ce513d2585e24a815c4fb5c4a93087f7f
Fernando E. Amado-Pupo <139940887+feamado@users.noreply.github.com>
2024-04-20 20:31:54 -0400

    Update data_access_and_class_instantiation.py

--- data_access_and_class_instantiation.py
+++ data_access_and_class_instantiation.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 class Star:
-    def __init__(self, id, id_type, luminosity, x0, y0, z0,dist, app_size):
+    def __init__(self, id, id_type, luminosity, x0, y0, z0, dist, app_size):
         self.id = id
         self.id_type = id_type
         self.name = f"{self.id}:{self.id_type}"
@@ -13,9 +13,14 @@ class Star:
         self.x0 = float(x0)
         self.y0 = float(y0)
         self.z0 = float(z0)
+        cos_th = self.z0 / (np.sqrt(np.sum(np.square([x0,y0,z0]))))
+        tan_phi = self.y0 / self.x0
+        self.phi = np.arctan(tan_phi)
+        self.theta = np.arcsin(cos_th)
         self.app_size = float(app_size)
         self.distance = dist
         self.coords = f"{self.x0}, {self.y0}, {self.z0}"
+
     def get_luminosity(self):
         return float(self.luminosity)
 
@@ -24,53 +29,103 @@ class Star:
 
     def get_distance(self):
         return float(self.distance)
+
     def get_name(self):
         return self.name
 
 
 class MaxHeap:
-    def __init__(self):
+    def __init__(self, mode):
         self.heap = [None]
+        self.mode = mode
 
     def insert(self, x):
         self.heap.append(x)
         i = len(self.heap) - 1
 
-        while i != 1:
-            parent = i // 2
-            tmp = self.heap[i]
-            if self.heap[i] > self.heap[parent]:
-                self.heap[i], self.heap[parent], i = self.heap[parent], tmp, parent
-            else:
-                break
-        return
+        if self.mode == "lum":
+
+            while i != 1:
+                parent = i // 2
+                tmp = self.heap[i]
+                if self.heap[i].luminosity > self.heap[parent].luminosity:
+                    self.heap[i], self.heap[parent], i = self.heap[parent], tmp, parent
+                else:
+                    break
+        elif self.mode == "app_size":
+            self.heap.append(x)
+            i = len(self.heap) - 1
+
+            while i != 1:
+                parent = i // 2
+                tmp = self.heap[i]
+                if self.heap[i].app_size > self.heap[parent].app_size:
+                    self.heap[i], self.heap[parent], i = self.heap[parent], tmp, parent
+                else:
+                    break
+        else:
+
+            while i != 1:
+                parent = i // 2
+                tmp = self.heap[i]
+                if self.heap[i].app_size > self.heap[parent].app_size:
+                    self.heap[i], self.heap[parent], i = self.heap[parent], tmp, parent
+                else:
+                    break
+            return
 
     def pop(self):
-
-        if len(self.heap) == 1:
-            return None
-        popped = self.heap[1]
-        self.heap[1], self.heap[len(self.heap) - 1] = self.heap[len(self.heap) - 1], self.heap[1]
-        del self.heap[len(self.heap) - 1]
-        if len(self.heap) == 1:
-            return popped
-
-        self.heap[1], self.heap[len(self.heap) - 1] = self.heap[len(self.heap) - 1], self.heap[1]
-
-        i = 1
-        end = len(self.heap)
-        while True:
-            left = 2 * i
-            right = 2 * i + 1
-
-            if left < end and self.heap[i] < self.heap[left]:
-                self.heap[left], self.heap[i] = self.heap[i], self.heap[left]
-                i = left
-            elif right < end and self.heap[i] < self.heap[right]:
-                self.heap[right], self.heap[i] = self.heap[i], self.heap[right]
-                i = right
-            else:
+        if self.mode == "lum":
+            if len(self.heap) == 1:
+                return None
+            popped = self.heap[1]
+            self.heap[1], self.heap[len(self.heap) - 1] = self.heap[len(self.heap) - 1], self.heap[1]
+            del self.heap[len(self.heap) - 1]
+            if len(self.heap) == 1:
                 return popped
+
+            self.heap[1], self.heap[len(self.heap) - 1] = self.heap[len(self.heap) - 1], self.heap[1]
+
+            i = 1
+            end = len(self.heap)
+            while True:
+                left = 2 * i
+                right = 2 * i + 1
+
+                if left < end and self.heap[i].luminosity < self.heap[left].luminosity:
+                    self.heap[left], self.heap[i] = self.heap[i], self.heap[left]
+                    i = left
+                elif right < end and self.heap[i].luminosity < self.heap[right].luminosity:
+                    self.heap[right], self.heap[i] = self.heap[i], self.heap[right]
+                    i = right
+                else:
+                    return popped
+
+        elif self.mode == "app_size":
+            if len(self.heap) == 1:
+                return None
+            popped = self.heap[1]
+            self.heap[1], self.heap[len(self.heap) - 1] = self.heap[len(self.heap) - 1], self.heap[1]
+            del self.heap[len(self.heap) - 1]
+            if len(self.heap) == 1:
+                return popped
+
+            self.heap[1], self.heap[len(self.heap) - 1] = self.heap[len(self.heap) - 1], self.heap[1]
+
+            i = 1
+            end = len(self.heap)
+            while True:
+                left = 2 * i
+                right = 2 * i + 1
+
+                if left < end and self.heap[i].app_size < self.heap[left].app_size:
+                    self.heap[left], self.heap[i] = self.heap[i], self.heap[left]
+                    i = left
+                elif right < end and self.heap[i].app_size < self.heap[right].app_size:
+                    self.heap[right], self.heap[i] = self.heap[i], self.heap[right]
+                    i = right
+                else:
+                    return popped
 
     def __str__(self):
         out = f"{self.heap[1]}"
@@ -81,12 +136,10 @@ class MaxHeap:
         return out
 
 
-def create_star_data_heap():
+def create_star_data_heap(mode):
     df = pd.read_csv('hyg_v37.csv')
-    out = MaxHeap()
 
-    select_cols = ["id","hip","hd","hr","gl","bf","x","y","z","mag","dist","lum",""]
-    #find id
+    # find id
     name = ""
     id_type = ""
     x = 0.0
@@ -96,14 +149,14 @@ def create_star_data_heap():
     dist = 0.0
     lum = 0.0
     app_size = 0
-    out = []
-    for index,row in df.iterrows():
+    out = MaxHeap(mode)
+    for index, row in df.iterrows():
 
         if row["hip"] == "":
             if row["hd"] == "":
                 if row["hr"] == "":
-                    if row["gl"] =="":
-                        if row["bf"] =="":
+                    if row["gl"] == "":
+                        if row["bf"] == "":
                             name = row["id"]
                             id_type = "dbid"
                         else:
@@ -130,13 +183,8 @@ def create_star_data_heap():
         lum = row["lum"]
         app_size = row["mag"]
 
-        inp = Star(name,id_type,lum,x,y,z,dist,app_size)
-        out.append(inp)
+        inp = Star(name, id_type, lum, x, y, z, dist, app_size)
+        out.insert(inp)
     return out
 
-
-A = create_star_data_heap()
-
-
-
-
+create_star_data_heap("lum")
